cbc518879bb6e9b4350f8fbe4a083a52bd65e8f3
Max Rivera <maxalejandrorh@hotmail.com>
2022-03-10 19:13:11 -0700

    organize 2

--- Organizer/Nave.py
+++ Organizer/Nave.py
@@ -1,22 +1,23 @@
 #La nomenclatura indica que la primera letra del nombre de una clase va en mayusculas
 from OpenGL.GL import *
 from glew_wish import *
-from Bala import *
 import glfw
 import math
+from Bala import *
 
-class Nave:
-    posicion_x = 0.0
-    posicion_y = 0.0
-    posicion_z = 0.0
+class Nave(Modelo):
     velocidad = 1.2
     angulo = 0.0
     velocidad_rotacion = 270.0
     fase = 90.0
-    bala = Bala()
+    balas = [Bala(), Bala(), Bala(), Bala(), Bala()]
     estado_anterior_espacio = glfw.RELEASE
 
     def dibujar(self):
+        
+        for bala in self.balas:
+            bala.dibujar()
+
         glPushMatrix()
         glTranslatef(self.posicion_x, self.posicion_y, self.posicion_z)
         glRotatef(self.angulo, 0.0, 0.0, 1.0)
@@ -25,7 +26,6 @@ class Nave:
         glVertex3f(-0.05,-0.05,0)
         glVertex3f(0.0,0.05,0)
         glVertex3f(0.05,-0.05,0)
-
         glEnd()
 
         glBegin(GL_LINE_LOOP)
@@ -38,10 +38,8 @@ class Nave:
 
         glPopMatrix()
 
-        self.bala.draw()
 
-    def actualizar(self, window, tiempo_delta):   
-
+    def actualizar(self, window, tiempo_delta ):
         #Leer los estados de las teclas que queremos
         estado_tecla_arriba = glfw.get_key(window, glfw.KEY_UP)
         estado_tecla_derecha = glfw.get_key(window, glfw.KEY_RIGHT)
@@ -49,13 +47,14 @@ class Nave:
         estado_tecla_espacio = glfw.get_key(window, glfw.KEY_SPACE)
 
         if (estado_tecla_espacio == glfw.PRESS and 
-            self.estado_anterior_espacio == glfw.RELEASE):
-                if not self.bala.disparando:
-                    self.bala.disparando = True
-                    self.bala.posicion_x = self.posicion_x
-                    self.bala.posicion_y = self.posicion_y
-                    self.bala.angulo = self.angulo + self.fase
-                    
+             self.estado_anterior_espacio == glfw.RELEASE):
+             for bala in self.balas:
+                if not bala.disparando:
+                    bala.disparando = True
+                    bala.posicion_x = self.posicion_x
+                    bala.posicion_y = self.posicion_y
+                    bala.angulo = self.angulo + self.fase
+                    break
 
         #Revisamos estados y realizamos acciones
         cantidad_movimiento = self.velocidad * tiempo_delta
@@ -76,7 +75,7 @@ class Nave:
             self.angulo = self.angulo - cantidad_rotacion
             if self.angulo < 0.0:
                 self.angulo = self.angulo + 360.0
-        estado_anterior_espacio = estado_tecla_espacio
+
 
         if self.posicion_x > 1.05: 
             self.posicion_x = -1.0
@@ -88,4 +87,7 @@ class Nave:
         if self.posicion_y < -1.05: 
             self.posicion_y = 1.0  
 
-        self.bala.actualizar(tiempo_delta)
+        self.estado_anterior_espacio = estado_tecla_espacio
+
+        for bala in self.balas:
+            bala.actualizar(tiempo_delta)
